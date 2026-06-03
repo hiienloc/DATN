9bd50213645017daec1bf2746c03fb989a655a76
hiienloc <locthibichhien2003@gmail.com>
2026-06-03 20:02:40 +0700

    Update database_setup.sql table definitions to match exact schemas with nullable constraints and correct nvarchar types

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -1,4 +1,4 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
 BEGIN
     CREATE TABLE [__EFMigrationsHistory] (
         [MigrationId] nvarchar(150) NOT NULL,
@@ -14,7 +14,7 @@ GO
 CREATE TABLE [Categories] (
     [CategoryId] int NOT NULL IDENTITY,
     [CategoryName] nvarchar(50) NOT NULL,
-    [IsActive] bit NOT NULL,
+    [IsActive] bit NULL,
     CONSTRAINT [PK_Categories] PRIMARY KEY ([CategoryId])
 );
 GO
@@ -23,15 +23,15 @@ CREATE TABLE [Packages] (
     [PackageId] int NOT NULL IDENTITY,
     [PackageCode] nvarchar(50) NOT NULL,
     [PackageName] nvarchar(50) NOT NULL,
-    [ImageUrl] varchar(255) NOT NULL,
+    [ImageUrl] varchar(255) NULL,
     [Description] nvarchar(255) NOT NULL,
     [Price] decimal(18,2) NOT NULL,
-    [StartDate] datetime2 NOT NULL,
-    [EndDate] datetime2 NOT NULL,
-    [Discount] decimal(18,2) NOT NULL,
-    [MaxQuantity] int NOT NULL,
-    [RowVersion] rowversion NOT NULL,
-    [IsActive] bit NOT NULL,
+    [StartDate] datetime2 NULL,
+    [EndDate] datetime2 NULL,
+    [Discount] decimal(18,2) NULL,
+    [MaxQuantity] int NULL,
+    [RowVersion] rowversion NULL,
+    [IsActive] bit NULL,
     CONSTRAINT [PK_Packages] PRIMARY KEY ([PackageId])
 );
 GO
@@ -48,8 +48,8 @@ CREATE TABLE [Products] (
     [ProductCode] nvarchar(50) NOT NULL,
     [ProductName] nvarchar(100) NOT NULL,
     [Unit] nvarchar(50) NOT NULL,
-    [IsActive] bit NOT NULL,
-    [CategoryId] int NOT NULL,
+    [IsActive] bit NULL,
+    [CategoryId] int NULL,
     CONSTRAINT [PK_Products] PRIMARY KEY ([ProductId]),
     CONSTRAINT [FK_Products_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories] ([CategoryId]) ON DELETE NO ACTION
 );
@@ -61,9 +61,9 @@ CREATE TABLE [Users] (
     [Email] varchar(100) NOT NULL,
     [Password] varchar(255) NOT NULL,
     [PhoneNumber] varchar(10) NOT NULL,
-    [CreateAt] datetime2 NOT NULL,
-    [IsActive] bit NOT NULL,
-    [RoleId] int NOT NULL,
+    [CreateAt] datetime2 NULL,
+    [IsActive] bit NULL,
+    [RoleId] int NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY ([UserId]),
     CONSTRAINT [FK_Users_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([RoleId]) ON DELETE NO ACTION
 );
@@ -71,14 +71,14 @@ GO
 
 CREATE TABLE [Forecasts] (
     [ForecastId] int NOT NULL IDENTITY,
-    [ProductId] int NOT NULL,
-    [ForecastType] varchar(50) NOT NULL,
-    [AvgDailySales] decimal(18,2) NOT NULL,
-    [PredictQuantity] decimal(18,2) NOT NULL,
-    [SuggestReStock] decimal(18,2) NOT NULL,
-    [CurrentStock] decimal(18,2) NOT NULL,
-    [ForecastDate] datetime2 NOT NULL,
-    [GeneratedAt] datetime2 NOT NULL,
+    [ProductId] int NULL,
+    [ForecastType] varchar(50) NULL,
+    [AvgDailySales] decimal(18,2) NULL,
+    [PredictQuantity] decimal(18,2) NULL,
+    [SuggestStock] decimal(18,2) NULL,
+    [CurrentStock] decimal(18,2) NULL,
+    [ForecastDate] datetime2 NULL,
+    [GeneratedAt] datetime2 NULL,
     CONSTRAINT [PK_Forecasts] PRIMARY KEY ([ForecastId]),
     CONSTRAINT [FK_Forecasts_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([ProductId]) ON DELETE NO ACTION
 );
@@ -86,10 +86,10 @@ GO
 
 CREATE TABLE [Inventories] (
     [InventoryId] int NOT NULL IDENTITY,
-    [ProductId] int NOT NULL,
+    [ProductId] int NULL,
     [QtyInStock] decimal(18,2) NOT NULL,
     [MinStock] decimal(18,2) NOT NULL,
-    [LastUpdated] datetime2 NOT NULL,
+    [LastUpdated] datetime2 NULL,
     [RowVersion] rowversion NOT NULL,
     CONSTRAINT [PK_Inventories] PRIMARY KEY ([InventoryId]),
     CONSTRAINT [FK_Inventories_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([ProductId]) ON DELETE NO ACTION
@@ -98,8 +98,8 @@ GO
 
 CREATE TABLE [PackageItems] (
     [PackageItemId] int NOT NULL IDENTITY,
-    [PackageId] int NOT NULL,
-    [ProductId] int NOT NULL,
+    [PackageId] int NULL,
+    [ProductId] int NULL,
     [PackageQty] decimal(18,2) NOT NULL,
     CONSTRAINT [PK_PackageItems] PRIMARY KEY ([PackageItemId]),
     CONSTRAINT [FK_PackageItems_Packages_PackageId] FOREIGN KEY ([PackageId]) REFERENCES [Packages] ([PackageId]) ON DELETE NO ACTION,
@@ -109,8 +109,8 @@ GO
 
 CREATE TABLE [Carts] (
     [CartId] int NOT NULL IDENTITY,
-    [TotalPrice] decimal(18,2) NOT NULL,
-    [UserId] int NOT NULL,
+    [TotalPrice] decimal(18,2) NULL,
+    [UserId] int NULL,
     CONSTRAINT [PK_Carts] PRIMARY KEY ([CartId]),
     CONSTRAINT [FK_Carts_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([UserId]) ON DELETE NO ACTION
 );
@@ -118,15 +118,15 @@ GO
 
 CREATE TABLE [Orders] (
     [OrderId] int NOT NULL IDENTITY,
-    [OrderCode] varchar(50) NOT NULL,
+    [OrderCode] varchar(50) NULL,
     [ReceiveName] nvarchar(50) NOT NULL,
     [ReceivePhone] varchar(10) NOT NULL,
     [ReceiveAddress] nvarchar(255) NOT NULL,
     [ShipmentPrice] decimal(18,2) NOT NULL,
-    [OrderDate] datetime2 NOT NULL,
-    [OrderStatus] nvarchar(50) NOT NULL,
-    [TotalAmount] decimal(18,2) NOT NULL,
-    [UserId] int NOT NULL,
+    [OrderDate] datetime2 NULL,
+    [OrderStatus] nvarchar(50) NULL,
+    [TotalAmount] decimal(18,2) NULL,
+    [UserId] int NULL,
     CONSTRAINT [PK_Orders] PRIMARY KEY ([OrderId]),
     CONSTRAINT [FK_Orders_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([UserId]) ON DELETE NO ACTION
 );
@@ -134,11 +134,11 @@ GO
 
 CREATE TABLE [InventoryTransactions] (
     [TransactionId] int NOT NULL IDENTITY,
-    [InventoryId] int NOT NULL,
+    [InventoryId] int NULL,
     [PackageId] int NULL,
     [QuantityChange] decimal(18,2) NOT NULL,
-    [Note] nvarchar(100) NOT NULL,
-    [TransactionDate] datetime2 NOT NULL,
+    [Note] nvarchar(50) NOT NULL,
+    [TransactionDate] datetime2 NULL,
     [TransactionType] nvarchar(max) NOT NULL,
     CONSTRAINT [PK_InventoryTransactions] PRIMARY KEY ([TransactionId]),
     CONSTRAINT [FK_InventoryTransactions_Inventories_InventoryId] FOREIGN KEY ([InventoryId]) REFERENCES [Inventories] ([InventoryId]) ON DELETE NO ACTION,
@@ -148,10 +148,10 @@ GO
 
 CREATE TABLE [CartItems] (
     [CartItemId] int NOT NULL IDENTITY,
-    [CartId] int NOT NULL,
-    [CartQuantity] int NOT NULL,
-    [CartPrice] decimal(18,2) NOT NULL,
-    [PackageId] int NOT NULL,
+    [CartId] int NULL,
+    [CartQuantity] int NULL,
+    [CartPrice] decimal(18,2) NULL,
+    [PackageId] int NULL,
     CONSTRAINT [PK_CartItems] PRIMARY KEY ([CartItemId]),
     CONSTRAINT [FK_CartItems_Carts_CartId] FOREIGN KEY ([CartId]) REFERENCES [Carts] ([CartId]) ON DELETE NO ACTION,
     CONSTRAINT [FK_CartItems_Packages_PackageId] FOREIGN KEY ([PackageId]) REFERENCES [Packages] ([PackageId]) ON DELETE NO ACTION
@@ -160,10 +160,10 @@ GO
 
 CREATE TABLE [OrderItems] (
     [OrderItemId] int NOT NULL IDENTITY,
-    [OrderId] int NOT NULL,
-    [PackageId] int NOT NULL,
-    [OrderPrice] decimal(18,2) NOT NULL,
-    [OrderQuantity] int NOT NULL,
+    [OrderId] int NULL,
+    [PackageId] int NULL,
+    [OrderPrice] decimal(18,2) NULL,
+    [OrderQuantity] int NULL,
     CONSTRAINT [PK_OrderItems] PRIMARY KEY ([OrderItemId]),
     CONSTRAINT [FK_OrderItems_Orders_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [Orders] ([OrderId]) ON DELETE NO ACTION,
     CONSTRAINT [FK_OrderItems_Packages_PackageId] FOREIGN KEY ([PackageId]) REFERENCES [Packages] ([PackageId]) ON DELETE NO ACTION
@@ -172,11 +172,11 @@ GO
 
 CREATE TABLE [Payments] (
     [PaymentId] int NOT NULL IDENTITY,
-    [OrderId] int NOT NULL,
-    [PaymentMethod] varchar(50) NOT NULL,
-    [Amount] decimal(18,2) NOT NULL,
-    [PaymentStatus] varchar(50) NOT NULL,
-    [CreatedAt] datetime2 NOT NULL,
+    [OrderId] int NULL,
+    [PaymentMethod] varchar(50) NULL,
+    [Amount] decimal(18,2) NULL,
+    [PaymentStatus] varchar(50) NULL,
+    [CreatedAt] datetime2 NULL,
     [UpdatedAt] datetime2 NULL,
     CONSTRAINT [PK_Payments] PRIMARY KEY ([PaymentId]),
     CONSTRAINT [FK_Payments_Orders_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [Orders] ([OrderId]) ON DELETE NO ACTION
@@ -185,13 +185,13 @@ GO
 
 CREATE TABLE [PaymentTransactions] (
     [TransactionId] int NOT NULL IDENTITY,
-    [PaymentId] int NOT NULL,
-    [VnpayTxnRef] varchar(50) NULL,
+    [PaymentId] int NULL,
+    [VnpayTxnRef] varchar(100) NULL,
     [TransactionNo] varchar(50) NULL,
     [BankCode] varchar(20) NULL,
     [ResponseCode] varchar(10) NULL,
-    [Status] varchar(50) NOT NULL,
-    [TransactionDate] datetime2 NOT NULL,
+    [Status] varchar(50) NULL,
+    [TransactionDate] datetime2 NULL,
     CONSTRAINT [PK_PaymentTransactions] PRIMARY KEY ([TransactionId]),
     CONSTRAINT [FK_PaymentTransactions_Payments_PaymentId] FOREIGN KEY ([PaymentId]) REFERENCES [Payments] ([PaymentId]) ON DELETE NO ACTION
 );
@@ -254,4 +254,3 @@ GO
 
 COMMIT;
 GO
-
